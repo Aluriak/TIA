a3732069bf69bad35ef6d0905a7b3a366cb658fe
Aluriak <lucas.bourneuf@laposte.net>
2015-07-07 21:38:29 +0200

    Coords can compute distance between instances and are iterable on x,y

--- tia/coords.py
+++ tia/coords.py
@@ -1,4 +1,4 @@
-
+import math
 
 
 class Coords:
@@ -15,5 +15,15 @@ class Coords:
 
     def __str__(self):
         return '(' + str(self.x) + ';' + str(self.y) + ')'
+
+    def __iter__(self):
+        return iter((self.x, self.y))
+
+    def square_distance_to(self, othr):
+        assert(isinstance(othr, Coords))
+        return (othr.x - self.x)**2 + (othr.y - self.y)**2
+
+    def distance_to(self, othr):
+        return math.sqrt(self.square_distance_to(othr))
 
 
