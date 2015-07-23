6655afdb3817c738123822b8ba47ceadafe90f62
Aluriak <lucas.bourneuf@laposte.net>
2015-07-23 20:39:44 +0200

    use the tia.commons.module_classes(2) instead of an endemic script

--- tia/mixins/__init__.py
+++ tia/mixins/__init__.py
@@ -6,30 +6,9 @@ Import automatically all mixins defined in files in the directory.
 # import all classes in mixins directory
 import os
 import importlib
-from functools import partial
-from tia.info  import PACKAGE_NAME
+from tia.commons import module_classes
 
-# all files in this directory
-modules = (
-    __name__ + '.' + name
-    for name, ext in (
-        os.path.splitext(filename)
-        for filename in os.listdir(__name__.replace('.', '/'))
-    )
-    if ext == '.py' and not name.startswith('__')
-)
-
-# declare a new function, shortcut to the importlib one
-imported = partial(importlib.import_module, package=PACKAGE_NAME)
-
-# import all classes in the modules
-globals().update({
-    n:cls
-    for module in modules
-    for n, cls in imported(module).__dict__.items()
-    # cls is a class, defined in the tia.mixins package
-    if type(cls) is type and __name__ in cls.__module__
-})
+globals().update(module_classes(__name__))
 
 
 
