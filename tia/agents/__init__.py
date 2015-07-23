52576460aab794dbe64f3ffb725cdd9b17ce48d2
Aluriak <lucas.bourneuf@laposte.net>
2015-07-23 20:41:44 +0200

    agent init have an automatic class import

--- tia/agents/__init__.py
+++ tia/agents/__init__.py
@@ -1,2 +1,8 @@
+from tia.commons      import module_classes
+# these import is necessary, because all others use it
 from tia.agents.agent import Agent
-from tia.agents.squad import Squad
+from tia.agents.troop import Troop
+
+globals().update(module_classes(__name__))
+
+
