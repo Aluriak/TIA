b334fff7808f9f7318b16a3eb09f43c6e50dc8ae
Aluriak <lucas.bourneuf@laposte.net>
2015-07-23 22:13:46 +0200

    add player __str__ method

--- tia/player.py
+++ tia/player.py
@@ -10,4 +10,11 @@ class Player:
     """
 
     def __init__(self):
-        self.name, self.shortname = ressources.random_name()
+        self.name, self.shortname = ressources.random_player_name()
+        # superpower activated
+        if self.name == 'The flying bus shelter':
+            self.name      = 'Our God, The Flying Bus Shelter'
+            self.shortname = 'FBS'
+
+    def __str__(self):
+        return 'Player ' + self.shortname + ' ' + self.name
