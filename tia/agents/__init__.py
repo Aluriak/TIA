e2eacd43714fd0e636dd6803c056d1a7bccb77bd
Aluriak <lucas.bourneuf@laposte.net>
2016-07-29 22:23:35 +0200

    agents: replace dynamic search by static, clearer, simpler

--- tia/agents/__init__.py
+++ tia/agents/__init__.py
@@ -2,7 +2,9 @@ from tia.commons      import module_classes
 # these import is necessary, because all others use it
 from tia.agents.agent import Agent
 from tia.agents.troop import Troop
+from tia.agents.squad import Squad
 
-globals().update(module_classes(__name__))
+TROOPS = (Squad,)
+# globals().update(module_classes(__name__))
 
 
