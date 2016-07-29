6bad219b50d4e16b33f7adb6c914d27a25a5d590
Aluriak <lucas.bourneuf@laposte.net>
2016-07-29 22:27:45 +0200

    mixins: movable holds the unit move implementation

--- tia/mixins/movable.py
+++ tia/mixins/movable.py
@@ -1,24 +1,50 @@
 """
 """
 from tia.coords   import Coords
-from tia.commands import MoveCommand
 
 
 class Movable:
     """
+    An agent is movable if its coordinates
+    can be modified at each update in order to reach
+    a defined target coordinates.
+
+    Note that the mixin Placable is necessary in order
+    to Movable mixin to work properly.
     """
 
-    def __init__(self, speed=0.1, target=None):
+    def __init__(self, speed=1.0, target=None):
         self.target = None
         self.speed  = speed
-        assert(speed >= 0.0)
+        assert speed >= 0.0
+        assert self.placable
+
+    def update(self, _):
+        """Move at each step towards its target"""
+        self.move()
+
+    def move(self):
+        """Operate a step for the unit move."""
+        if self.target_reached:
+            return  # nothing to do
+        def sign(x):
+            return 1 if x > 0 else (-1 if x < 0 else 0)
+        self.coords = self.coords + Coords(
+            self.speed * sign(self.target.x - self.coords.x),
+            self.speed * sign(self.target.y - self.coords.y),
+        )
 
     @property
-    def movable(self): return True
+    def movable(self):
+        return True
+
+    @property
+    def target_reached(self):
+        """True if unit is on its target, or if no target"""
+        return self.target is None or self.target == self.coords
 
     def __str__(self):
         if self.target is None:
             return '[MOVABLE]'
         else:
             return '[MOVING TO ' + str(self.target) + ']'
-
