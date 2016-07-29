e19ee637b1c4e7d1c817cee7e4392ff1ce7e5714
Aluriak <lucas.bourneuf@laposte.net>
2016-07-29 14:30:13 +0200

    main: mocking showing the 1.2.3 pyglet bug

--- tia/__main__.py
+++ tia/__main__.py
@@ -5,25 +5,63 @@ from tia.coords         import Coords
 from tia.engine         import Engine
 from tia.commands       import PrintCommand, MoveCommand, QuitCommand
 from tia.gui            import WorldView, TerminalManagementInterface
-from random             import randint
 import tia.commons as commons
+import random
+import time
 
 
 LOGGER = commons.logger()
 
 
-# initialization
-engine = Engine(Coords(800, 600))
-engine.start()  # thread start
-gui  = WorldView(engine)
-term = TerminalManagementInterface(engine, 'lucas')
-engine.register_observer(gui)
+def test_whole():
+    # initialization
+    engine = Engine(Coords(800, 600))
+    engine.start()  # thread start
+    gui  = WorldView(engine)
+    term = TerminalManagementInterface(engine, 'lucas')
+    engine.register_observer(gui)
 
-gui.start()
-term.run()  # block
-
-
-gui.join()
-engine.join()  # wait the end
+    gui.start()
+    term.run()  # block
 
 
+    gui.join()
+    engine.join()  # wait the end
+
+
+
+
+def test_report():
+    class MockAgent():
+        def __init__(self):
+            self.coords = random.randint(10, 500), random.randint(10, 500)
+    class MockEngine():
+        def __init__(self):
+            self.agents = [MockAgent() for _ in range(10)]
+            self.agents = []
+        def add_command(self, command):
+            print('Engine receive', command)
+        def agents_with(self, whatever):
+            return self.agents
+        def agents_at(self, coords, precision=1.0):
+            try:
+                return iter([random.choice(self.agents)])
+            except IndexError:
+                return None
+    engine = MockEngine()
+    engine.space_width = 800
+    engine.space_height= 600
+
+    # create and test the world view object
+    wv = WorldView(engine)
+    wv.start()
+    for _ in range(10):
+        wv.update({
+            'new_report': MockAgent()
+        })
+        time.sleep(0.4)
+
+
+if __name__ == "__main__":
+    # test_whole()
+    test_report()
