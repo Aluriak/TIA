e3d51b5964b9e20f0f93bdb319d9d0f1d1d62e8f
Aluriak <lucas.bourneuf@laposte.net>
2016-07-29 22:28:27 +0200

    report: holds a priority

--- tia/report.py
+++ tia/report.py
@@ -6,20 +6,33 @@ from tia.coords import Coords
 from tia.agents import Agent
 import tia.time_scheduler as time
 
+from enum import Enum
+
+
+class Priority(Enum):
+    """Two Report priorities level:
+    - high is for "must be sent now"
+    - normal is for "should be sent with the next report"
+
+    """
+    high = 3
+    normal = 2
 
 
 class Report:
     """
     """
 
-    def __init__(self, unit, text=''):
+    def __init__(self, unit, text='', priority=Priority.normal):
         self.unit = unit
         self.text = str(text)
         self.time = time.time()
+        self.priority = priority
         # deductions from previous data
         self.coords = unit.coords
 
     def __str__(self):
         return 'Report of ' + str(self.unit) + ': ' + self.text
 
-
+    def __gt__(self, othr):
+        return self.priority > othr.priority
